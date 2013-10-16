93a9e08802176c267ecd39cd6cb183362c8e1f14
Hackbright Student <user@mini-006.(none)>
2013-10-15 18:02:43 -0700

    changed methods to POST

--- webapp2.py
+++ webapp2.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import hackbright_app
 
 app = Flask(__name__)
@@ -31,49 +31,53 @@ def get_project_info():
 def add_new_student():
     return render_template("add_new_student.html")
 
-@app.route("/create_student_record")
+@app.route("/create_student_record", methods=["POST"])
 def create_student_record():
     hackbright_app.connect_to_db()
-    student_first = request.args.get("first_name")
-    student_last = request.args.get("last_name")
-    student_github = request.args.get("student_github")
+    student_first = request.form.get("first_name")
+    student_last = request.form.get("last_name")
+    student_github = request.form.get("student_github")
  
     hackbright_app.make_new_student(student_first, student_last, student_github)
 
-    html = render_template("added_successfully.html")
-    return html
+    # html = render_template("added_successfully.html")
+    # return html
+
+    return redirect("/student?github=%s"%student_github)
 
 @app.route("/add_new_project")
 def add_new_project():
     return render_template("add_new_project.html")
 
-@app.route("/create_project_record")
+@app.route("/create_project_record", methods=["POST"])
 def create_project_record():
     hackbright_app.connect_to_db()
-    project_title = request.args.get("title")
-    project_description = request.args.get("description")
-    project_maxgrade = request.args.get("max_grade")
+    project_title = request.form.get("title")
+    project_description = request.form.get("description")
+    project_maxgrade = request.form.get("max_grade")
  
     hackbright_app.make_new_project(project_title, project_description, project_maxgrade)
 
-    html = render_template("added_successfully.html")
-    return html
+    #html = render_template("added_successfully.html")
+    #return html
+    return redirect("/projects?project_title=%s"%project_title)
 
 @app.route("/assign_student_grade")
 def assign_student_grade():
     return render_template("assign_student_grade.html")
 
-@app.route("/assign_grade")
+@app.route("/assign_grade", methods=["POST"])
 def assign_grade():
     hackbright_app.connect_to_db()
-    student_github = request.args.get("github")
-    project = request.args.get("project_title")
-    student_grade = request.args.get("grade")
+    student_github = request.form.get("github")
+    project = request.form.get("project_title")
+    student_grade = request.form.get("grade")
 
     hackbright_app.assign_grade(student_github, project, student_grade)
 
-    html = render_template("added_successfully.html")
-    return html
+    # html = render_template("added_successfully.html")
+    # return html
+    return redirect("/student?github=%s"%student_github)
 
 if __name__ == "__main__":
     app.run(debug=True)
